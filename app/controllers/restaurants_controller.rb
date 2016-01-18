c3a63dd4f671bf48a0c111fe0f24ae08f7486549
James Borrell <james.borrell92@gmail.com>
2016-01-18 17:09:25 +0000

    Add reviews to restaurant, tests passing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,40 @@
 class RestaurantsController < ApplicationController
+	def index
+		@restaurants = Restaurant.all
+	end
+
+	def create
+		Restaurant.create(restaurant_params)
+		redirect_to '/restaurants'
+	end
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name)
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update(restaurant_params)
+		redirect_to '/restaurants'
+	end
+
+	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.destroy
+		flash[:notice] = 'Restaurant deleted successfully'
+		redirect_to '/restaurants'
+	end
 end
+
